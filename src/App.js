31096159ccc239b14cf9290230f8d9c65cbbb4f7
Aylin DeBruyne <adebruyne88@gmail.com>
2018-07-24 13:58:14 -0400

    begin initial repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header>
+        <h1>React Notes App</h1>
+        <input type="text" placeholder="Imagine It"/>
+      </header>
+         </div>
+    );
+  }
+}
+
+export default App;
